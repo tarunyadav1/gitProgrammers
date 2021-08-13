bf9822fd4f1a687d47f221cb541cba0a0d6bfa1f
Tarun Yadav <tarunyadav9761@gmail.com>
2021-08-13 16:57:10 +0530

    start function

--- rebase.js
+++ rebase.js
@@ -1,5 +1,7 @@
 // write a function that take person name, age and job as input and print them in reverse order
 
-function rebase(name, age) {
+function rebase(name, age, job) {
   console.log(name + " " + age + " " + job);
 }
+
+rebase("John", "25", "Software Engineer");
